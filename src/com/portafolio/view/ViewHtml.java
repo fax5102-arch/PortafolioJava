5cd51aaae44bbc3adf18e62cb95488ed173be3c2
fax5102-arch <fax5102@gmail.com>
2026-09-05 13:39:52 -0500

    Update ViewHtml.java
    hola

--- src/com/portafolio/view/ViewHtml.java
+++ src/com/portafolio/view/ViewHtml.java
@@ -11,7 +11,7 @@ public class ViewHtml {
     public static String renderPortafolio(List<Evidencia> listaEvidencias, boolean autenticado) {
         StringBuilder evidenciasHtml = new StringBuilder();
         if (listaEvidencias.isEmpty()) {
-            evidenciasHtml.append("<p style=\"font-size: 0.88rem;\">No hay evidencias publicadas aún.</p>");
+            evidenciasHtml.append("<p style=\"font-size: 0.88rem;\">Hola.</p>");
         } else {
             Map<String, List<Evidencia>> evidenciasPorSemana = listaEvidencias.stream()
                     .collect(Collectors.groupingBy(Evidencia::getSemana, LinkedHashMap::new, Collectors.toList()));
